1d7c79630daa8ea3767b0cdd50b3fd4c50e11856
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2024-02-06 00:23:26 +0800

    configure server to connect to db

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
-	api := ServerAPI{}
+	var conn *pgx.Conn
+	var err error
+
+	for retry := 0; retry < 5; retry++ {
+		conn, err = pgx.Connect(context.Background(),
+			"postgres://postgres:password@db:5432/postgres")
+		if err == nil {
+			break
+		}
+
+		fmt.Fprintf(os.Stderr, "Unable to connect to database. Retrying in 5 seconds. Error: %v\n", err)
+		time.Sleep(5 * time.Second)
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to connect to database after multiple retries: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer conn.Close(context.Background())
+	api := ServerAPI{DB: conn}
 	r := chi.NewRouter()
 	r.Mount("/api/v1/", Handler(api))
 	fmt.Println("Running server")
--- server.go
+++ server.go
@@ -4,18 +4,31 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"gorm.io/gorm"
+	"github.com/jackc/pgx/v5"
 )
 
 type ServerAPI struct {
-	DB *gorm.Model
+	DB *pgx.Conn
 }
 
 // (GET /todos)
 func (server ServerAPI) FindTodos(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
 	var todoList []Todo
-	todoList = append(todoList, Todo{})
+	err := server.DB.QueryRow(
+		r.Context(),
+		`SELECT id, title, date, content FROM useless_todo.todo_list`).Scan(&todoList)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		errRes := Error{
+			Code:    http.StatusInternalServerError,
+			Message: "Error occured during DB query",
+		}
+		json.NewEncoder(w).Encode(errRes)
+		return
+	}
+
 	by, _ := json.Marshal(todoList)
 	w.Write(by)
 }
